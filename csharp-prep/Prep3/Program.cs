b143aa0027c073d3eb99e1616bad70a58eaa6c91
mmusih <hub22001@byui.edu>
2024-06-13 14:17:07 +0200

    week1.5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+Random randomGenerator = new Random();
+int number = randomGenerator.Next(1, 101);
+// Console.WriteLine(number);
+int guess = -1;
+while (!(guess == number)) {
+    Console.WriteLine("Guess the magic number?");
+    string response = Console.ReadLine();
+    guess = int.Parse(response);
+    if ( guess > number){
+        Console.WriteLine("Lower");
+    } 
+    else if (guess < number){
+        Console.WriteLine("Higher");
+    }
+    else {
+        Console.WriteLine("You guessed it!!");
+    }
+        }
+
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int number = -1;
+        int sum = 0;
+        List<int> numbers = new List<int>();
+        while (!(number == 0)) {
+            Console.WriteLine("Enter number: ");
+            string response = Console.ReadLine();
+            number = int.Parse(response);
+            if (!(number == 0)) {
+                numbers.Add(number);
+            }
+        }
+        for (int i = 0; i<numbers.Count; i++)
+        sum += numbers[i];
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int high = numbers.Max();
+        Console.WriteLine($"The largest number is: {high}");
     }
 }
